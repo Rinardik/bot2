3215b3bfc92517f0d43094735c6d89555ffca29d
Rinard Ahmetzanov <rinardahetzyanov@yandex.ru>
2024-05-19 15:57:48 +0300

    first commit

---
+++ bot2.0.py
@@ -0,0 +1,57 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+import os
+
+bot = Bot(os.getenv('TOKEN'))
+dp = Dispatcher(bot=bot)
+
+main = ReplyKeyboardMarkup(resize_keyboard=True)
+main.add('Каталог').add('Корзина').add('Контакты')
+
+main_admin = ReplyKeyboardMarkup(resize_keyboard=True)
+main_admin.add('Каталог').add('Корзина').add('Контакты').add('Админ-панель')
+
+admin_panel = ReplyKeyboardMarkup(resize_keyboard=True)
+admin_panel.add('Добавить товар').add('Удалить товар').add('Сделать рассылку')
+
+catalog_list = InlineKeyboardMarkup(row_width=2)
+catalog_list.add(InlineKeyboardButton(text='Футболки', url=''),
+                 InlineKeyboardButton(text='Шорты', url=''),
+                 InlineKeyboardButton(text='Кроссовки', url=''))
+
+
+@dp.message_handler(commands=['id'])
+async def cmd_id(message: types.Message):
+    await message.answer(f'{message.from_user.id}')
+
+
+@dp.message_handler(text='Каталог')
+async def catalog(message: types.Message):
+    await message.answer(f'Каталог пуст!', reply_markup=catalog_list)
+
+
+@dp.message_handler(text='Корзина')
+async def cart(message: types.Message):
+    await message.answer(f'Корзина пуста!')
+
+
+@dp.message_handler(text='Контакты')
+async def contacts(message: types.Message):
+    await message.answer(f'Покупать товар у него: ')
+
+
+@dp.message_handler(text='Админ-панель')
+async def contacts(message: types.Message):
+    if message.from_user.id == int(os.getenv('ADMIN_ID')):
+        await message.answer(f'Вы вошли в админ-панель', reply_markup=admin_panel)
+    else:
+        await message.reply('Я тебя не понимаю.')
+
+
+@dp.message_handler()
+async def answer(message: types.Message):
+    await message.reply('Я тебя не понимаю.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
